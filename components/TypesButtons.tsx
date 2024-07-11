773ef3d69818df2ef874f914414af7cf57278d70
Maxime-Labbe <maxime30labbe@gmail.com>
2024-07-11 16:03:21 +0200

    feat(Types) : Added component to make links to types

---
+++ components/TypesButtons.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import style from '../styles/components.module.css'
+import Link from 'next/link'
+
+interface TypesButtonsProps {
+    label: string
+    url: string
+}
+
+const TypesButtons: React.FC<{ items: TypesButtonsProps[] }> = ({ items }) => {
+    const [currentUrl, setCurrentUrl] = useState('')
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            setCurrentUrl(window.location.href)
+        }
+    }, [])
+    return (
+        <>
+            <h1 className={style.md}>
+                Ajouter / Supprimer le(s) type(s) suivant(s)
+            </h1>
+            <div className={style.typeButtons}>
+                {items.map(item => (
+                    <Link
+                        key={item.label}
+                        href={`${currentUrl}/${item.url}`}
+                        className={style.submit}
+                    >
+                        {item.label}
+                    </Link>
+                ))}
+            </div>
+        </>
+    )
+}
+
+export default TypesButtons
