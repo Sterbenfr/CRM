8567b60b7ea4e5fdfe3088d12db2e1b47e36c815
Alexis <150680244+Alexis-aka-Yazm@users.noreply.github.com>
2024-07-15 10:14:15 +0200

    Update1 Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,109 +2,111 @@ import styles from '../styles/Footer.module.css'
 
 export default function Footer() {
     return (
-        <footer className={styles.footer}>
-            <p>--- Fait par ENIGMA ---</p>
-            <ul>
-                <li>
-                    Aurelien Schmieder{' '}
-                    <a href='https://github.com/Aurelienschmi' target='_blank'>
-                        <div className={styles.pop}>
-                            <img src='/IMG/github.svg' alt='GitHub' />
-                        </div>
-                    </a>
-                    <a
-                        href='https://www.linkedin.com/in/aurelien-schmieder-0017391ab/'
-                        target='_blank'
-                    >
-                        <div className={styles.pop}>
-                            <img src='/IMG/linkedin.svg' alt='LinkedIn' />
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    Alexis Hazebrouck{' '}
-                    <a
-                        href='https://github.com/Alexis-aka-Yazm'
-                        target='_blank'
-                    >
-                        <div className={styles.pop}>
-                            <img src='/IMG/github.svg' alt='GitHub' />
-                        </div>
-                    </a>
-                    <a
-                        href='https://www.linkedin.com/in/alexis-hazebrouck-3b7a7a296'
-                        target='_blank'
-                    >
-                        <div className={styles.pop}>
-                            <img src='/IMG/linkedin.svg' alt='LinkedIn' />
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    Hugo Matyla{' '}
-                    <a href='https://github.com/Fenerz07' target='_blank'>
-                        <div className={styles.pop}>
-                            <img src='/IMG/github.svg' alt='GitHub' />
-                        </div>
-                    </a>
-                    <a
-                        href='https://www.linkedin.com/in/hugo-matyla/'
-                        target='_blank'
-                    >
-                        <div className={styles.pop}>
-                            <img src='/IMG/linkedin.svg' alt='LinkedIn' />
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    Mathis Dacacio{' '}
-                    <a href='https://github.com/MathisDacacio' target='_blank'>
-                        <div className={styles.pop}>
-                            <img src='/IMG/github.svg' alt='GitHub' />
-                        </div>
-                    </a>
-                    <a
-                        href='https://www.linkedin.com/in/mathis-dacacio-298a25293/'
-                        target='_blank'
-                    >
-                        <div className={styles.pop}>
-                            <img src='/IMG/linkedin.svg' alt='LinkedIn' />
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    Maxime Labbe{' '}
-                    <a href='https://github.com/Maxime-Labbe' target='_blank'>
-                        <div className={styles.pop}>
-                            <img src='/IMG/github.svg' alt='GitHub' />
-                        </div>
-                    </a>
-                    <a
-                        href='https://www.linkedin.com/in/maxime-labbe-626012293/'
-                        target='_blank'
-                    >
-                        <div className={styles.pop}>
-                            <img src='/IMG/linkedin.svg' alt='LinkedIn' />
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    Pierre Caudreliez{' '}
-                    <a href='https://github.com/Sterbenfr' target='_blank'>
-                        <div className={styles.pop}>
-                            <img src='/IMG/github.svg' alt='GitHub' />
-                        </div>
-                    </a>
-                    <a
-                        href='https://www.linkedin.com/in/pierre-caudreliez/'
-                        target='_blank'
-                    >
-                        <div className={styles.pop}>
-                            <img src='/IMG/linkedin.svg' alt='LinkedIn' />
-                        </div>
-                    </a>
-                </li>
-            </ul>
-        </footer>
+            <div className={styles.foot}>
+                <footer className={styles.footer}>
+                    <p>--- Fait par ENIGMA ---</p>
+                    <ul>
+                        <li>
+                            Aurelien Schmieder{' '}
+                            <a href='https://github.com/Aurelienschmi' target='_blank'>
+                                <div className={styles.pop}>
+                                    <img src='/IMG/github.svg' alt='GitHub' />
+                                </div>
+                            </a>
+                            <a
+                                href='https://www.linkedin.com/in/aurelien-schmieder-0017391ab/'
+                                target='_blank'
+                            >
+                                <div className={styles.pop}>
+                                    <img src='/IMG/linkedin.svg' alt='LinkedIn' />
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            Alexis Hazebrouck{' '}
+                            <a
+                                href='https://github.com/Alexis-aka-Yazm'
+                                target='_blank'
+                            >
+                                <div className={styles.pop}>
+                                    <img src='/IMG/github.svg' alt='GitHub' />
+                                </div>
+                            </a>
+                            <a
+                                href='https://www.linkedin.com/in/alexis-hazebrouck-3b7a7a296'
+                                target='_blank'
+                            >
+                                <div className={styles.pop}>
+                                    <img src='/IMG/linkedin.svg' alt='LinkedIn' />
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            Hugo Matyla{' '}
+                            <a href='https://github.com/Fenerz07' target='_blank'>
+                                <div className={styles.pop}>
+                                    <img src='/IMG/github.svg' alt='GitHub' />
+                                </div>
+                            </a>
+                            <a
+                                href='https://www.linkedin.com/in/hugo-matyla/'
+                                target='_blank'
+                            >
+                                <div className={styles.pop}>
+                                    <img src='/IMG/linkedin.svg' alt='LinkedIn' />
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            Mathis Dacacio{' '}
+                            <a href='https://github.com/MathisDacacio' target='_blank'>
+                                <div className={styles.pop}>
+                                    <img src='/IMG/github.svg' alt='GitHub' />
+                                </div>
+                            </a>
+                            <a
+                                href='https://www.linkedin.com/in/mathis-dacacio-298a25293/'
+                                target='_blank'
+                            >
+                                <div className={styles.pop}>
+                                    <img src='/IMG/linkedin.svg' alt='LinkedIn' />
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            Maxime Labbe{' '}
+                            <a href='https://github.com/Maxime-Labbe' target='_blank'>
+                                <div className={styles.pop}>
+                                    <img src='/IMG/github.svg' alt='GitHub' />
+                                </div>
+                            </a>
+                            <a
+                                href='https://www.linkedin.com/in/maxime-labbe-626012293/'
+                                target='_blank'
+                            >
+                                <div className={styles.pop}>
+                                    <img src='/IMG/linkedin.svg' alt='LinkedIn' />
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            Pierre Caudreliez{' '}
+                            <a href='https://github.com/Sterbenfr' target='_blank'>
+                                <div className={styles.pop}>
+                                    <img src='/IMG/github.svg' alt='GitHub' />
+                                </div>
+                            </a>
+                            <a
+                                href='https://www.linkedin.com/in/pierre-caudreliez/'
+                                target='_blank'
+                            >
+                                <div className={styles.pop}>
+                                    <img src='/IMG/linkedin.svg' alt='LinkedIn' />
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </footer>
+            </div>
     )
 }
