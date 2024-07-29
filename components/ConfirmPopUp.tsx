6d5c651d28f79b3aa0b08ba5902a45d2e690a368
root <root@PC>
2024-07-29 15:36:18 +0200

    feat:adding a popUp to confirm the delete

---
+++ components/ConfirmPopUp.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import style from '../styles/components.module.css'
+
+interface ConfirmPopUpProps {
+    message: string
+    onConfirm: () => void
+    onCancel: () => void
+}
+
+const ConfirmPopUp: React.FC<ConfirmPopUpProps> = ({
+    message,
+    onConfirm,
+    onCancel,
+}) => {
+    return (
+        <div className={style.popUpBackground}>
+            <div className={style.errorPopUp}>
+                <h2 className={style.NF}>Confirmation:</h2>
+                <p className={style.CNF}>{message}</p>
+                <div className={style.functionBlock}>
+                    <button className={style.BTN_CNF} onClick={onConfirm}>
+                        Confirmer
+                    </button>
+                    <button className={style.BTN_CNF} onClick={onCancel}>
+                        Annuler
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ConfirmPopUp
